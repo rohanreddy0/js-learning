ffdc8f770437a5eb3697ade83f2399044e0ad685
Rohan <rohanreddy027@gmail.com>
2024-09-20 18:22:26 +0100

    Added win logic

--- rockpaperscissors_js/script.js
+++ rockpaperscissors_js/script.js
@@ -16,5 +16,36 @@ function getHumanChoice(){
     return humanChoice = prompt("Choose rock, paper or scissors").toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        return "It is a draw !";
+    }
 
-console.log(getHumanChoice());
+    // Rock - Paper
+    else if (humanChoice === "rock" && computerChoice === "paper"){
+        return "Computer wins - paper beats rock";
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock"){
+        return "You win - paper beats rock";
+    }
+
+    // Rock - Scissors
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+        return "You win - rock beats scissors";
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock"){
+        return "Computer wins - rock beats scissors";
+    }
+
+    // Paper-Scissors
+    else if (humanChoice === "paper" && computerChoice === "scissors"){
+        return "Computer wins - scissors beats paper";
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper"){
+        return "You win - scissors beats paper";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
